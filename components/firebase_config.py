2b92add6677c8c0f13e282e585c8dd826076a993
0Sheraz <121394436+0Sheraz@users.noreply.github.com>
2025-10-19 20:05:43 +0500

    FIX: Demo auth mode for deployment

--- components/firebase_config.py
+++ components/firebase_config.py
@@ -1,16 +1,10 @@
 import streamlit as st
 
-firebaseConfig = {
-    "apiKey": "AIzaSyCAgDpTsp4XUCyWwsQ-BKq35eWd3LiajWk",
-    "authDomain": "earthscape-auth-5501a.firebaseapp.com",
-    "databaseURL": "https://earthscape-auth-5501a-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "earthscape-auth-5501a",
-    "storageBucket": "earthscape-auth-5501a.firebasestorage.app",
-    "messagingSenderId": "981413608442",
-    "appId": "1:981413608442:web:3a27f181f0ed650ef6c243"
-}
+class DemoAuth:
+    def sign_in_with_email_and_password(self, email, password):
+        return {"idToken": "demo-token", "localId": "demo-user", "email": email}
+    
+    def create_user_with_email_and_password(self, email, password):
+        return {"idToken": "demo-token", "localId": "demo-user", "email": email}
 
-firebase = st(firebaseConfig)
-auth = firebase.auth()
-db = firebase.database()
-storage = firebase.storage()
+auth = DemoAuth()
